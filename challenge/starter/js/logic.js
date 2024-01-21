2bac64cc9901fd331dfeaab33cbf10fc5cb3614e
Khubaib Ul Haque <khubaib_12@hotmail.com>
2024-01-21 21:15:29 +0000

    - adding message if saving appointment was successfull

--- challenge/starter/js/logic.js
+++ challenge/starter/js/logic.js
@@ -1,44 +1,45 @@
 var currentDate = dayjs().format('dddd DD, MMMM YYYY');
 $("#currentDay").text(currentDate);
 
-
 function setBlockTime() {
-    var currentTime = 12; // to test for textarea colour classes
+    var currentTime = 12; // to test for textarea color classes
+    // var currentTime = dayjs().hour();
 
     var startTime = dayjs().startOf('day').add(9, 'hour'); // Start from 9 AM
     var endTime = dayjs().startOf('day').add(18, 'hour'); // End at 5 PM
 
-    //for loop to create each timeblock based on time I have set from 9-5
+    // for loop to create each time block based on time set from 9-5
     for (var hour = startTime; hour.isBefore(endTime); hour = hour.add(1, 'hour')) {
-        (function (currentHour) {
-            var timeBlock = $('<div>').addClass('row time-block');
-            // set the time format to hours
-            var hourSection = $('<div>').addClass('col-1 hour').text(currentHour.format('h A'));
-            var textarea = $('<textarea>').addClass('col-9 description');
+        var timeBlock = $('<div>').addClass('row time-block');
+        // set the time format to hours
+        var hourSection = $('<div>').addClass('col-1 hour').text(hour.format('h A'));
+        var textarea = $('<textarea>').addClass('col-9 description');
 
-            // Check if the current hour is in the past, present, or future
-            if (currentHour.hour() < currentTime) {
-                textarea.addClass('past');
-            } else if (currentHour.hour() === currentTime) {
-                textarea.addClass('present');
-            } else {
-                textarea.addClass('future');
-            }
+        // Check if the current hour is in the past, present, or future
+        if (hour.hour() < currentTime) {
+            textarea.addClass('past');
+        } else if (hour.hour() === currentTime) {
+            textarea.addClass('present');
+        } else {
+            textarea.addClass('future');
+        }
 
-            var saveBtn = $('<button>').addClass('col-1 saveBtn').append($('<i>').addClass('fas fa-save hover'));
+        var saveBtn = $('<button>').addClass('col-1 saveBtn').append($('<i>').addClass('fas fa-save hover'));
 
-            saveBtn.on('click', function () {
-                var savedText = textarea.val();
-                var localStorageApp = {"user text": savedText, "Hour": currentHour.format('h A')}
-                localStorage.setItem("appointment", JSON.stringify(localStorageApp));
-            });
+        // Event listener for save button
+        saveBtn.on('click', function () {
+            var savedText = textarea.val();
+            var localStorageApp = { "user text": savedText, "Hour": hour.format('h A') };
+            localStorage.setItem("appointment", JSON.stringify(localStorageApp));
+            // A success message outside the loop
+            var successMessage = $('<div>').addClass('align-item').text('testinggggggggggg');
+            $('.container').prepend(successMessage);
+        });
 
-            timeBlock.append(hourSection, textarea, saveBtn);
-            $('.container').append(timeBlock);
-        })(hour);
+        timeBlock.append(hourSection, textarea, saveBtn);
+        $('.container').append(timeBlock);
     }
+
 }
 
 setBlockTime();
-
-
