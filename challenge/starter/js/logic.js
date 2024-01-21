6d74360400383377962275d20a2752a5d8674bbb
Khubaib Ul Haque <khubaib_12@hotmail.com>
2024-01-21 00:02:43 +0000

    - created a function to render the blocks -created a function to loop through times from 9am to 5pm and create a block for each hour

--- challenge/starter/js/logic.js
+++ challenge/starter/js/logic.js
@@ -1,7 +1,48 @@
 
-var currentDate = dayjs().format('dddd, d,  MMMM YYYY')
-
+var currentDate = dayjs().format('dddd DD, MMMM YYYY');
 $("#currentDay").text(currentDate);
 
-dayjs().second(30).valueOf() // => new Date().setSeconds(30)
-dayjs().second() // => new Date().getSeconds()
+
+
+// var textarea = $("<div>");
+// textarea.addClass("textarea")
+// textarea.text('hellooo')
+
+// var description = $("div")
+// description.addClass("description")
+// description.text("heloooooooooooooooo")
+
+// $(".container").append(textarea)
+    // Create a time block
+    function createHourBlock(hour) {
+        var timeBlock = $('<div>').addClass('row time-block');
+    
+        // Create the time/hour section
+        var hourSection = $('<div>').addClass('col-1 hour').text(hour.format('H A'));
+        timeBlock.append(hourSection);
+    
+        // Create the textarea
+        var textarea = $('<textarea>').addClass('col-9 description present');
+        timeBlock.append(textarea);
+    
+        // Create the save button
+        var saveBtn = $('<button>').addClass('col-1 btn saveBtn');
+        saveBtn.append($('<i>').addClass('fas fa-save'));
+        timeBlock.append(saveBtn);
+    
+        // Append the time block to the container
+        $('.container').append(timeBlock);
+    }
+    
+    function setBlockTime(){
+
+        var startTime = dayjs().startOf('day').add(9, 'hour'); // Start from 9 AM
+        var endTime = dayjs().startOf('day').add(18, 'hour'); // End at 5 PM
+    
+        for (var hour = startTime; hour.isBefore(endTime); hour = hour.add(1, 'hour')) {
+            createHourBlock(hour);
+        }
+
+    }
+    setBlockTime()
+    
